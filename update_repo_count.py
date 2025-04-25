745107e09c01a42b771fd68d443fe1848b807bae
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-25 12:48:48 -0400

    i think were good

---
+++ update_repo_count.py
@@ -0,0 +1,15 @@
+import urllib.request
+import json
+
+with urllib.request.urlopen('https://api.github.com/users/redrain0o0') as api:
+    apireq = json.load(api)
+
+
+with open('README.md','r') as f:
+  content = f.readlines()
+
+
+content[5] = "@          @:          :@          @  |  Repos: " + str(apireq["public_repos"]) + "\n"
+
+with open('README.md','w') as f:
+  f.writelines(content)
