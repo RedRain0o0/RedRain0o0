2ece05767aeb6482b99a14e39f59ecc94ed041a6
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-25 10:18:04 -0400

    store age to json

--- update_age.py
+++ update_age.py
@@ -7,6 +7,9 @@ with open('README.md','r') as f:
   content = f.read()
 
 #var_dic.update(age=(var_dic.get(age)+1))
+#var_dic["age"] + 1
+
+jsoncontent = '{"age": ' + var_dic["age"] + '}'
 
 with open('readme.json','w') as f:
   f.write(var_dic)
