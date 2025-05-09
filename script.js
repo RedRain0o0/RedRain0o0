1e679023b4c47fc4bdb45b221bf08f381ca0cffb
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-05-09 12:01:48 -0400

    attempt to setup bsky account

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+funtion onLoad() {
+  getBskyProfile();
+}
+
 function getBskyProfile() {
   const url = "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfiles?actors=did:plc:lzylocxb3cr2hhpjejy2d3qp";
-  const accountsDiv = document.getElementById('accounts');
+  const accountsSpan = document.getElementById('accountsSpan');
   fetch(url)
   .then(function(response) {
     response.json().then(function(account){
       bsky = account.profiles[0];
       console.log("Account %s (@%s) has %s followers and %s following", bsky.displayName, bsky.handle, bsky.followersCount, bsky.followsCount);
+      container = document.createElement("div")
+
+      container.classList = "account";
+      container.id = "bskyAccount"
+
+      icon = document.createElement("img");
+      icon.src = bsky.avatar;
+      icon.classList = "accountAvatar"
+      container.appendChild(icon);
+
+      link = document.createElement("a");
+      link.classList = "accountName";
+      link.innerText = bsky.displayName
+      link.href = "https://bsky.app/profile/" + bsky.handle;
+      container.appendChild(link);
+
+      desc = document.createElement("p");
+      desc.innerText = project.description;
+      desc.classList = "accountDescription"
+      container.appendChild(desc)
+
+      accountsSpan.appendChild(container);
     });
   }).catch(err => console.error(err));
 }
